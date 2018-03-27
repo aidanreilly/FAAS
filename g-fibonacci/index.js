1e4b1a7c585fe87f7d38dcd3d596e518d5127276
aidan.reilly <aidareilly@gmail.com>
2018-03-28 00:10:44 +0100

    commit new

---
+++ g-fibonacci/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+exports.http = (event, context, callback) => {
+  const fibo = fib()
+  for (let i=0; i<30; i++){
+    console.log(fibo())
+  } 
+  const result = {
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "headers": {},
+    "body": "done"
+  }
+
+  function fib() {
+  let x = 0
+  let y = 1
+  return function () {    
+    const temp = x;
+    x = x + y;
+    y = temp;
+    return y
+  }
+}
+
+  callback(null, result);
+};
+
+exports.event = (event, callback) => {
+  callback();
+};
