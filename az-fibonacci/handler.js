d1d9da50c072828b0add3990b162e7bef5453a84
aidan.reilly <aidareilly@gmail.com>
2018-03-29 23:47:29 +0100

    update to hello world again.

--- az-fibonacci/handler.js
+++ az-fibonacci/handler.js
@@ -2,7 +2,7 @@
 
 /* eslint-disable no-param-reassign */
 
-module.exports.calcFibonacci = function (context) {
+module.exports.cFibonacci = function (context) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   context.res = {
