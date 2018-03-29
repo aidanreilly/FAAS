6ab81131b2bf7a4b61b2a23eb0d8941f2c70040f
aidan.reilly <aidareilly@gmail.com>
2018-03-29 22:29:11 +0100

    this is the base setup that works as a hello world.

--- az-fibonacci/handler.js
+++ az-fibonacci/handler.js
@@ -2,12 +2,12 @@
 
 /* eslint-disable no-param-reassign */
 
-module.exports.hello = function (context) {
+module.exports.calcFibonacci = function (context) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: 'Go Serverless v1.x! Your function executed successfully!',
+    body: 'Argh',
   };
 
   context.done();
