924c32ca9faf5b9db4f703e4f04541e3e0a89ccb
aidan.reilly <aidareilly@gmail.com>
2018-05-05 19:34:15 +0100

    init gcloud recur

---
+++ g-fibonacci-recur/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.http = (request, response) => {
+
+  var fibOut = [];
+  for (let i=0; i<40; i++){
+    console.log(fib(i));
+    fibOut.push(fib(i))
+  } 
+
+  function fib(n) {
+    if(n > 1){
+      return fib(n-1) + fib(n-2)
+    } else {
+      return n;
+    }
+  }
+
+  response.status(200).send('Calculated Fibonacci to 40 places using recursion, the final calculated value is: ' + String(fibOut[39]));
+};
+
+exports.event = (event, callback) => {
+  callback();
+};
